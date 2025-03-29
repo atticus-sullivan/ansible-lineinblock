be9cc19f6ac2509b9380ae51b6225a152aa003fb
Lukas <oss.heindl+git@protonmail.com>
2025-03-29 23:56:20 +0100

    add validate parameter similar to lineinfile

--- plugins/modules/lineinblock.py
+++ plugins/modules/lineinblock.py
@@ -72,6 +72,7 @@ extends_documentation_fragment:
   - action_common_attributes
   - action_common_attributes.files
   - files
+  - validate
 attributes:
   check_mode:
     support: full
@@ -243,23 +244,19 @@ def write_changes(module, lines, dest):
     with os.fdopen(tmpfd, 'wb') as f:
         f.writelines([line.encode('utf-8') if isinstance(line, str) else line for line in lines])
 
-    # TODO validate parameter
-    # validate = module.params.get('validate', None)
-    # valid = not validate
-    # if validate:
-    #     if "%s" not in validate:
-    #         module.fail_json(msg="validate must contain %%s: %s" % (validate))
-    #     (rc, out, err) = module.run_command(to_bytes(validate % tmpfile, errors='surrogate_or_strict'))
-    #     valid = rc == 0
-    #     if rc != 0:
-    #         module.fail_json(msg='failed to validate: '
-    #                              'rc:%s error:%s' % (rc, err))
-    # if valid:
-    #     module.atomic_move(tmpfile,
-    #                        to_native(os.path.realpath(to_bytes(dest, errors='surrogate_or_strict')), errors='surrogate_or_strict'),
-    #                        unsafe_writes=module.params['unsafe_writes'])
-
-    module.atomic_move(tmpfile, to_native(os.path.realpath(to_bytes(dest, errors='surrogate_or_strict')), errors='surrogate_or_strict'), unsafe_writes=module.params['unsafe_writes'])
+    validate = module.params.get('validate', None)
+    valid = not validate
+    if validate:
+        if "%s" not in validate:
+            module.fail_json(msg=f"validate must contain %s: {validate}")
+        (rc, out, err) = module.run_command(to_bytes(validate % tmpfile, errors='surrogate_or_strict'))
+        valid = rc == 0
+        if rc != 0:
+            module.fail_json(msg=f'failed to validate: rc:{rc} error:{err}')
+    if valid:
+        module.atomic_move(tmpfile,
+                           to_native(os.path.realpath(to_bytes(dest, errors='surrogate_or_strict')), errors='surrogate_or_strict'),
+                           unsafe_writes=module.params['unsafe_writes'])
 
 def check_file_attrs(module, changed, message, diff):
     """
